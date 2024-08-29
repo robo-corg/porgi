de984392f529b3e6321580d20b8a459890590e33
Andrew McHarg <amcharg@gmail.com>
2024-08-28 22:54:01 -0700

    feat: Use tables for displaying project list (#2)
    * feat: Use table for projects
    * fix: Format future modified times less silly

--- src/tui.rs
+++ src/tui.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Local, TimeDelta};
 use color_eyre::config::HookBuilder;
 use crossterm::{
     event::{Event, EventStream, KeyCode, KeyEventKind},
@@ -5,6 +6,7 @@ use crossterm::{
     ExecutableCommand,
 };
 use eyre::{bail, Result};
+use fancy_duration::FancyDuration;
 use futures::{future::FutureExt, select, StreamExt};
 use ratatui::{prelude::*, style::palette::tailwind, widgets::*};
 
@@ -43,8 +45,9 @@ impl Default for ColorConfig {
     }
 }
 
-struct StatefulList {
-    state: ListState,
+#[derive(Default)]
+struct StatefulTable {
+    state: TableState,
     items: ProjectStore,
     last_selected: Option<usize>,
 }
@@ -57,7 +60,7 @@ struct StatefulList {
 /// Check the drawing logic for items on how to specify the highlighting style for selected items.
 pub(crate) struct App {
     config: Arc<Config>,
-    items: StatefulList,
+    items: StatefulTable,
     project_events: ProjectLoader,
 }
 
@@ -94,7 +97,7 @@ impl App {
     pub(crate) fn new(config: Arc<Config>, project_events: ProjectLoader) -> Self {
         Self {
             config,
-            items: StatefulList::new(),
+            items: StatefulTable::new(),
             project_events,
         }
     }
@@ -253,7 +256,7 @@ impl App {
             .fg(self.config.colors.text_color)
             .bg(self.config.colors.project_header_bg);
         let inner_block = Block::new()
-            .borders(Borders::NONE)
+            .borders(Borders::RIGHT)
             .fg(self.config.colors.text_color)
             .bg(self.config.colors.normal_row_color);
 
@@ -265,29 +268,98 @@ impl App {
         outer_block.render(outer_area, buf);
 
         // Iterate through all elements in the `items` and stylize them.
-        let items: Vec<ListItem> = self
+        // let items: Vec<ListItem> = self
+        //     .items
+        //     .items
+        //     .iter()
+        //     .map(|project| {
+        //         let text = Line::from(vec![
+        //             Span::raw(project.name.as_str()),
+        //             Span::raw(format!(" files: {}", project.file_count)),
+        //         ]);
+        //         ListItem::new(text)
+        //     })
+        //     .collect();
+
+        // Create a List from all list items and highlight the currently selected one
+        // let items = List::new(items)
+        //     .block(inner_block)
+        //     .highlight_style(
+        //         Style::default()
+        //             .add_modifier(Modifier::BOLD)
+        //             .add_modifier(Modifier::REVERSED)
+        //             .fg(self.config.colors.selected_style_fg),
+        //     )
+        //     .highlight_symbol(">")
+        //     .highlight_spacing(HighlightSpacing::Always);
+
+        let rows: Vec<Row> = self
             .items
             .items
             .iter()
-            .map(|project| ListItem::new(project.name.as_str()))
+            .map(|project| {
+                Row::new(vec![project.name.clone(), {
+                    let now: DateTime<Local> = Local::now();
+                    let date: DateTime<Local> = project.modified.into();
+                    let d = now.signed_duration_since(date);
+
+                    if d.abs() < TimeDelta::new(60, 0).unwrap() {
+                        "just now".to_string()
+                    } else if d.abs() > TimeDelta::new(48 * 60 * 60, 0).unwrap() {
+                        let date: DateTime<Local> = project.modified.into();
+                        date.format("%Y-%m-%d").to_string()
+                    } else if d >= TimeDelta::zero() {
+                        format!(
+                            "{} ago",
+                            FancyDuration::new(d).filter(&[
+                                fancy_duration::DurationPart::Days,
+                                fancy_duration::DurationPart::Hours,
+                                fancy_duration::DurationPart::Minutes,
+                            ])
+                        )
+                    } else {
+                        format!(
+                            "{} from now",
+                            FancyDuration::new(d.abs()).filter(&[
+                                fancy_duration::DurationPart::Days,
+                                fancy_duration::DurationPart::Hours,
+                                fancy_duration::DurationPart::Minutes,
+                            ])
+                        )
+                    }
+                }])
+            })
             .collect();
 
-        // Create a List from all list items and highlight the currently selected one
-        let items = List::new(items)
+        //let rows = [Row::new(vec!["Cell1", "Cell2"])];
+        // Columns widths are constrained in the same way as Layout...
+        let widths = [Constraint::Fill(1), Constraint::Length(16)];
+
+        let table = Table::new(rows, widths)
+            // ...and they can be separated by a fixed spacing.
+            .column_spacing(1)
+            // You can set the style of the entire Table.
+            .style(Style::new().blue())
+            // It has an optional header, which is simply a Row always visible at the top.
+            // It has an optional footer, which is simply a Row always visible at the bottom.
+            //.footer(Row::new(vec!["Updated on Dec 28"]))
+            // .footer(
+            // )
             .block(inner_block)
+            // The selected row and its content can also be styled.
             .highlight_style(
                 Style::default()
                     .add_modifier(Modifier::BOLD)
                     .add_modifier(Modifier::REVERSED)
                     .fg(self.config.colors.selected_style_fg),
             )
-            .highlight_symbol(">")
-            .highlight_spacing(HighlightSpacing::Always);
+            // ...and potentially show a symbol in front of the selection.
+            .highlight_symbol(">");
 
         // We can now render the item list
         // (look careful we are using StatefulWidget's render.)
         // ratatui::widgets::StatefulWidget::render as stateful_render
-        StatefulWidget::render(items, inner_area, buf, &mut self.items.state);
+        StatefulWidget::render(table, inner_area, buf, &mut self.items.state);
     }
 
     fn render_info(&self, project: &Project, area: Rect, buf: &mut Buffer) {
@@ -329,10 +401,10 @@ impl App {
     }
 }
 
-impl StatefulList {
+impl StatefulTable {
     fn new() -> Self {
-        StatefulList {
-            state: ListState::default(),
+        Self {
+            state: TableState::default(),
             items: ProjectStore::default(),
             last_selected: None,
         }
